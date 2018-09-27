df3fd37b18478e1cd8fc1557bf99ac12ee6beef7
zhu.yuxing@hotmail.com <zhu.yuxing@hotmail.com>
2018-09-27 22:45:42 +0900

    config

---
+++ yczd-server-config/src/main/java/com/yczd/server/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.yczd.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 
+ * @author yczd
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+}
