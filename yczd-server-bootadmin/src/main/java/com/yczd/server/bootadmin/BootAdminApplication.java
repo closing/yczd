b53abfa5b444a28b0cc62046be3fb352f6f400eb
closing <zhu.yuxing@hotmail.com>
2018-09-28 17:44:55 +0900

    Create BootAdminApplication.java

---
+++ yczd-server-bootadmin/src/main/java/com/yczd/server/bootadmin/BootAdminApplication.java
@@ -0,0 +1,17 @@
+package com.yczd.server.bootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableAdminServer
+public class BootAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootAdminApplication.class, args);
+	}
+
+}
