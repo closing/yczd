fb735be82fd067a3baec0b1550cc778cedb6c757
closing <zhu.yuxing@hotmail.com>
2018-10-05 17:21:52 +0900

    Create UserServiceImpl.java

---
+++ yczd-api-account/src/main/java/com/yczd/api/account/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.yczd.api.account.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yczd.api.account.domain.User;
+import com.yczd.api.account.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	@Override
+	public User findById(String id) {
+		return userRepository.findById(id);
+	}
+
+	@Override
+	public User findByName(String name) {
+		return userRepository.findByName(name);
+	}
+
+	@Override
+	public User save(User user) {
+		if (user.getId() == null) {
+			Integer id = userRepository.insert(user);
+			user.setId(id);
+		} else {
+
+		}
+		return null;
+	}
+
+	@Override
+	public void delete(User user) {
+
+	}
+
+	@Override
+	public void deleteById(String id) {
+
+	}
+
+}
