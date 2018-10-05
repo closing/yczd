054bb91e5a75b8e4e1f7a51040c032c4b69428b7
closing <zhu.yuxing@hotmail.com>
2018-10-05 17:16:34 +0900

    Create UserRepository.java

---
+++ yczd-api-account/src/main/java/com/yczd/api/account/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.yczd.api.account.repository;
+
+import java.util.List;
+
+import com.yczd.api.account.domain.User;
+
+/**
+ * 用户数据库操作接口类
+ * @author yczd
+ *
+ */
+public interface UserRepository {
+	public List<User> findAll();
+
+	public User findById(String id);
+
+	public User findByName(String userName);
+
+	public Integer insert(User user);
+
+	public void update(User user);
+
+	public void deleteById(String id);
+
+	public void delete(User user);
+}
