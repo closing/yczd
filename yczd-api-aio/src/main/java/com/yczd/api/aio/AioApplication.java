d1aff9c5353f827ad778f2aa35ef21a08b2cc2b4
closing <zhu.yuxing@hotmail.com>
2018-10-10 17:27:20 +0900

    Create AioApplication.java

---
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/AioApplication.java
@@ -0,0 +1,11 @@
+package com.yczd.api.aio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AioApplication.class, args);
+	}
+}
