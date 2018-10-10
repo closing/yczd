1f59bd59567f3daf9f59efa564f58fe016aa6187
closing <zhu.yuxing@hotmail.com>
2018-10-10 17:27:52 +0900

    Create DemoController.java

---
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/auth/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.yczd.api.aio.auth.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/demo")
+public class DemoController {
+
+	@GetMapping()
+	public List<String> demo() {
+		return Arrays.asList(new String[] { "demo" });
+	}
+
+}
