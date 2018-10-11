fa53927b849b3dc81c7278341e921ed37a147381
closing <zhu.yuxing@hotmail.com>
2018-10-11 17:18:27 +0900

    Create RootController.java

---
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/RootController.java
@@ -0,0 +1,27 @@
+package com.yczd.api.aio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+	@GetMapping()
+	public List<String> root() {
+		List<String> links = new ArrayList<>();
+
+		links.add("/v1/goods/");
+		links.add("/v1/categories/");
+		links.add("/v1/carts/");
+		links.add("/v1/orders/");
+		links.add("/v1/users/");
+
+		return links;
+
+	}
+}
