76930a4565bf423c9b1a099ba16d2fd6c1163c51
closing <zhu.yuxing@hotmail.com>
2018-10-11 17:23:29 +0900

    Update and rename yczd-api-aio/src/main/resources/db/migration/v0_1_ddl_init.sql to yczd-api-aio/src/main/resources/db/migration/h2/migrationv0_1_ddl_init.sql

--- yczd-api-aio/src/main/resources/db/migration/v0_1_ddl_init.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists user;
-
-create table user (id int primary key auto_increment, username varchar(20), password varchar(20));
-
-insert into user (username, password) values ('demo', 'password');
-insert into user (username, password) values ('dev', 'password');
-insert into user (username, password) values ('test', 'password');
-insert into user (username, password) values ('prod', 'password');
