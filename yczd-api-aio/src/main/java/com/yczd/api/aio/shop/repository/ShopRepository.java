c42be2b18f0c0cf581ea0f6106146460ab2f4fe9
closing <zhu.yuxing@hotmail.com>
2018-10-23 18:05:22 +0900

    Update ShopRepository.java

--- yczd-api-aio/src/main/java/com/yczd/api/aio/shop/repository/ShopRepository.java
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/shop/repository/ShopRepository.java
@@ -1,7 +1,20 @@
 package com.yczd.api.aio.shop.repository;
 
-import com.yczd.api.aio.shop.domain.Shop;
+import java.util.List;
+
+import com.yczd.api.aio.shop.model.Shop;
 
 public interface ShopRepository {
 	public Shop getShopById(Integer id);
+
+	public List<Shop> getShops();
+
+	public int insert(Shop shop);
+
+	public int update(Shop shop);
+
+	public int delete(Shop shop);
+
+	public int delete(Integer shopId);
+
 }
