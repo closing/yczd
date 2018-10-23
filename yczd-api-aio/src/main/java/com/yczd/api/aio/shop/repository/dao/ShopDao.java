8d27d6562a33c8dd9ba8999f4d8dde8659559348
closing <zhu.yuxing@hotmail.com>
2018-10-23 18:05:37 +0900

    Update ShopDao.java

--- yczd-api-aio/src/main/java/com/yczd/api/aio/shop/repository/dao/ShopDao.java
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/shop/repository/dao/ShopDao.java
@@ -1,14 +1,18 @@
 package com.yczd.api.aio.shop.repository.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
-import com.yczd.api.aio.shop.domain.Shop;
+import com.yczd.api.aio.shop.model.Shop;
 import com.yczd.api.aio.shop.repository.ShopRepository;
 import com.yczd.api.aio.shop.repository.dao.mapper.ShopMapper;
 
 @Repository
+@Scope("prototype")
 public class ShopDao implements ShopRepository {
 	@Autowired
 	private SqlSession sqlSession;
@@ -19,4 +23,29 @@ public class ShopDao implements ShopRepository {
 		return sqlSession.getMapper(ShopMapper.class).selectByPrimaryKey(id);
 	}
 
+	@Override
+	public List<Shop> getShops() {
+		return sqlSession.getMapper(ShopMapper.class).selectAll();
+	}
+
+	@Override
+	public int insert(Shop shop) {
+		return sqlSession.getMapper(ShopMapper.class).insert(shop);
+	}
+
+	@Override
+	public int update(Shop shop) {
+		return sqlSession.getMapper(ShopMapper.class).updateByPrimaryKey(shop);
+	}
+
+	@Override
+	public int delete(Shop shop) {
+		return delete(shop.getShopId());
+
+	}
+
+	@Override
+	public int delete(Integer shopId) {
+		return sqlSession.getMapper(ShopMapper.class).deleteByPrimaryKey(shopId);
+	}
 }
