48bb82361b6f95bbf908d94fa9f7eb46f7ffe64c
closing <zhu.yuxing@hotmail.com>
2018-10-24 17:49:49 +0900

    Create Auth2UserService.java

---
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/service/Auth2UserService.java
@@ -0,0 +1,7 @@
+package com.yczd.api.aio.auth2.sevice;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface Auth2UserService extends UserDetailsService {
+
+}
