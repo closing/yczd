823fa62cb97ce19f16ff5a969c71399ce6fddc7f
closing <zhu.yuxing@hotmail.com>
2018-10-24 17:49:05 +0900

    Create Auth2Controller.java

---
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/web/controller/Auth2Controller.java
@@ -0,0 +1,23 @@
+package com.yczd.api.aio.auth2.web.controller;
+
+//@RestController
+//@RequestMapping("/v1/oauth")
+public class Auth2Controller {
+
+//	@Autowired
+//	TokenStore tokenStore;
+//
+//	@RequestMapping("/info")
+//	public ResponseEntity<?> authInfo(@RequestHeader("Authorization") String auth) {
+//		MyUserDetails userDetails = (MyUserDetails) tokenStore.readAuthentication(auth.split(" ")[1]).getPrincipal();
+//
+//		AuthUser user = userDetails.getUser();
+//		if (user != null) {
+//			return ResponseEntity.ok().body(user.getUserName() + ":" + user.getPassword());
+//		} else {
+//			return ResponseEntity.notFound().build();
+//		}
+//
+//	}
+
+}
