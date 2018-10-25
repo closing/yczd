6c2424727b2331810cfcf69821fa719c6c20cc8e
closing <zhu.yuxing@hotmail.com>
2018-10-25 17:18:48 +0900

    Update Auth2UserDetails.java

--- yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/model/Auth2UserDetails.java
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/model/Auth2UserDetails.java
@@ -3,6 +3,12 @@ package com.yczd.api.aio.auth2.model;
 import lombok.Getter;
 import lombok.Setter;
 
+//TODO OAuth
+/**
+ *
+ * @author YCZD
+ *
+ */
 public class Auth2UserDetails
 {
 	@Getter
@@ -10,7 +16,6 @@ public class Auth2UserDetails
 	private com.yczd.api.aio.auth2.model.Auth2User user;
 
 	public Auth2UserDetails(com.yczd.api.aio.auth2.model.Auth2User user) {
-		//super(user.getUserName(), user.getPassword(), true, true, true, true, Collections.EMPTY_SET);
 		this.user = user;
 	}
 }
