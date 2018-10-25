5dd7ee8e1f21fe5c42ff5a9a81c0be11b211fe6a
closing <zhu.yuxing@hotmail.com>
2018-10-25 17:20:00 +0900

    Update MethodSecurityConfig.java

--- yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/security/MethodSecurityConfig.java
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/security/MethodSecurityConfig.java
@@ -1,13 +1,14 @@
 package com.yczd.api.aio.auth2.security;
 
+//TODO Auth
 //@Configuration
 //@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class MethodSecurityConfig //extends GlobalMethodSecurityConfiguration
 {
-//	@Autowired
-//	private OAuth2SecurityConfiguration securityConfig;
-//
-//	@Override
+	//@Autowired
+	//private OAuth2SecurityConfiguration securityConfig;
+
+	//@Override
 //	protected MethodSecurityExpressionHandler createExpressionHandler() {
 //		return new OAuth2MethodSecurityExpressionHandler();
 //	}
