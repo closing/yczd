2ea541668fe91c0d0a3f2196aec63fa567ae36c4
closing <zhu.yuxing@hotmail.com>
2018-10-26 17:56:40 +0900

    Update ShopController.java

--- yczd-api-aio/src/main/java/com/yczd/api/aio/shop/web/controller/ShopController.java
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/shop/web/controller/ShopController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.yczd.api.aio.shop.model.Shop;
+import com.yczd.api.aio.shop.model.entity.Shop;
 import com.yczd.api.aio.shop.service.ShopService;
 
 @RestController
