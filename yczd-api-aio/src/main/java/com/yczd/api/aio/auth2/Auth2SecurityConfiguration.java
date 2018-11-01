e034cad5ff52529f72bf260068a5fa37d3b5dafc
closing <zhu.yuxing@hotmail.com>
2018-11-01 18:16:58 +0900

    Update and rename yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/security/Auth2SecurityConfiguration.java to yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/Auth2SecurityConfiguration.java

--- yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/Auth2SecurityConfiguration.java
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/Auth2SecurityConfiguration.java
@@ -1,6 +1,10 @@
-package com.yczd.api.aio.auth2.security;
+package com.yczd.api.aio.auth2;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,35 +12,53 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import com.yczd.api.aio.auth2.service.impl.Auth2UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class Auth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	@Autowired
-	ClientDetailsService clientDetailsService;
+	@Bean
+	@ConfigurationProperties(value = "yczd.auth2.datasource")
+	public DataSource auth2DataSource() {
+		return DataSourceBuilder.create().driverClassName("com.mysql.jdbc.Driver")
+				.url("jdbc:mysql://10.207.30.152:3306/yczd_mini").password("admin").username("root").build();
+	}
+
+	//
+	//	@Autowired
+	//	UserDetailsService jdbcUserDetailsService;
+	//
+	@Bean
+	public ClientDetailsService clientDetailsService() {
+		return new JdbcClientDetailsService(auth2DataSource());
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new Auth2UserDetailsServiceImpl();
+	}
 
 	@Autowired
 	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("yczd1").password("{noop}password").roles("SHOP").and()
-				.withUser("yczd2").password("{noop}password").roles("USER").and()
-				.withUser("yczd3").password("{noop}password").roles("STATION").and()
-				.withUser("yczd4").password("{noop}password").roles("DRIVER").and()
-				.withUser("yczd").password("{noop}password").roles("ADMIN");
+		auth.userDetailsService(userDetailsService()).and();
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 				.anonymous().disable()
-				.authorizeRequests().antMatchers("/oauth/token").permitAll();
+				.authorizeRequests().antMatchers("/oauth/token").permitAll().and()
+				.userDetailsService(userDetailsService());
 
 	}
 
@@ -48,7 +70,7 @@ public class Auth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JdbcTokenStore(auth2DataSource());
 
 	}
 
@@ -57,9 +79,14 @@ public class Auth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore) {
 		TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
 		handler.setTokenStore(tokenStore);
-		handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
-		handler.setClientDetailsService(clientDetailsService);
+		handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService()));
+		handler.setClientDetailsService(clientDetailsService());
 		return handler;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsServiceBean());
 	}
 
 	@Bean
