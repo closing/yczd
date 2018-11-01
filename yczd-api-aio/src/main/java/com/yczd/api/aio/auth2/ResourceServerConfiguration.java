8058e0d417eaa392ab20e4fad142005876426db4
closing <zhu.yuxing@hotmail.com>
2018-11-01 18:18:17 +0900

    Update and rename yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/security/ResourceServerConfiguration.java to yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/ResourceServerConfiguration.java

--- yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/ResourceServerConfiguration.java
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/auth2/ResourceServerConfiguration.java
@@ -1,4 +1,4 @@
-package com.yczd.api.aio.auth2.security;
+package com.yczd.api.aio.auth2;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
