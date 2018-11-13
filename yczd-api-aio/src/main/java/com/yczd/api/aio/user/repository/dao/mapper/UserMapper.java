6a1ffb89f1a63a474a91c403fb4b3238be29a413
closing <zhu.yuxing@hotmail.com>
2018-11-13 17:27:46 +0900

    Create UserMapper.java

---
+++ yczd-api-aio/src/main/java/com/yczd/api/aio/user/repository/dao/mapper/UserMapper.java
@@ -0,0 +1,33 @@
+package com.yczd.api.aio.user.repository.dao.mapper;
+
+import com.yczd.api.aio.user.model.entity.User;
+import com.yczd.api.aio.user.model.entity.UserCriteria;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+public interface UserMapper {
+    long countByExample(UserCriteria example);
+
+    int deleteByExample(UserCriteria example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExampleWithRowbounds(UserCriteria example, RowBounds rowBounds);
+
+    List<User> selectByExample(UserCriteria example);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserCriteria example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserCriteria example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
